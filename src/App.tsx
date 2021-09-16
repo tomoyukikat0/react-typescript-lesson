e16cf3757041d0f4bb3cd66bd4db9b4ac7e9ce5a
Tomoyuki Kato <tomoyukikato@TomoyukinoMacBook-Pro.local>
2021-09-16 23:40:37 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const [status, setStatus] = useState<string | number>("text");
+  const [input, setInput] = useState("");
+  const [counter, setCounter] = useState(0);
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value)
+  }
+  useEffect(() => {
+    console.log("useEffect in App invoked!");
+    document.title = `current value is ${counter}`;
+  }, [counter]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h4>{status}</h4>
+        <button onClick={() => setStatus(1)}>Button</button>
+        <h4>{input}</h4>
+        <input type="text" value={input} onChange={onChangeHandler} />
+        <h4>{counter}</h4>
+        <button onClick={()=>setCounter((preCounter)=>preCounter+1)}>Increment</button>
         <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
